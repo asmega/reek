0a0c9d15ea490d86de01da92cbd07556a3af3f48
asmega <asmega@ph-lee.com>
2012-06-24 12:09:01 +0100

    autoload application

--- lib/reek.rb
+++ lib/reek.rb
@@ -4,3 +4,9 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), 'reek', 'version')
 require File.join(File.dirname(File.expand_path(__FILE__)), 'reek', 'examiner')
 require File.join(File.dirname(File.expand_path(__FILE__)), 'reek', 'smell_warning')
+
+module Reek
+  module Cli
+    autoload :Application, 'reek/cli/application'
+  end
+end
